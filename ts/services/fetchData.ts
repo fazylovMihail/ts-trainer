e427db4ddd4a9d037be3bd063b8ef242f5e8f165
fazylovMihail <fazylovmihail726@gmail.com>
2026-03-10 20:18:54 +0500

    default

--- ts/services/fetchData.ts
+++ ts/services/fetchData.ts
@@ -1,5 +1,5 @@
 export default async function  fetchData<T>(): Promise<T[]> {
-  const response = await fetch('../data/formuls.json');
+  const response = await fetch('./data/formuls.json');
   if(!response.ok) throw new Error(`Ошибка HTTP ${response.status}`);
 
   return await response.json();
